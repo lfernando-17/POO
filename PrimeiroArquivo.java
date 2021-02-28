9c513e05adc7e115a4e8220f730b438234d2fab1
Luiz Fernando Gomes <lfernando17@gmail.com>
2021-02-28 17:55:23 -0300

    first commit

---
+++ PrimeiroArquivo.java
@@ -0,0 +1,15 @@
+/*
+Nome : Luiz Fernando Amarino Gomes Leal
+Matricula : 0050016658
+*/
+class Main {
+    public static void main(String[] args) {
+      int i = 0 ; 
+      while (i<=10)
+      {
+        System.out.println(i*10);
+        i++;
+      }
+    }
+  }
+
