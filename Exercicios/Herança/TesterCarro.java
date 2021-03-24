895c0211af2c7ff19f2052331f802ecc843a62c6
Luiz Fernando Gomes <lfernando17@gmail.com>
2021-03-24 14:28:34 -0300

    Exercicio

---
+++ Exercicios/Herança/TesterCarro.java
@@ -0,0 +1,9 @@
+package Exercicios.Herança;
+
+public class TesterCarro {
+    public static void main(String[] args) {
+            BMW meuCarro = new BMW();
+
+            meuCarro.imprimir();
+    }
+}
